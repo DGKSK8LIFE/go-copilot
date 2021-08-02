8ee070a840e8b152eed4260b95f9ab00ed2822f5
Tarek Ali <tarekali15@outlook.com>
2021-08-02 09:26:26 -0700

    first few math examples

---
+++ examples/math/math.go
@@ -0,0 +1,14 @@
+// Write a function that returns the sum of the two numbers.
+func sum(a, b int) int {
+	return a + b
+}
+
+// Write a function that returns the product of the two numbers.
+func product(a, b int) int {
+	return a * b
+}
+
+// Write a function that returns the square of the number.
+func square(a int) int {
+	return a * a
+}
